364d402ead985250ea8f4ea44f5676016a59d63f
SistaGopalaKrishna <gopalakrishnasista@gmail.com>
2024-10-31 12:59:28 +0530

    chatapp

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:106064659572:web:8fe45f7eda14efa1083d21";
+  static String apiKey = "AIzaSyB8Kxf1TMNrNJt-ViuepTZdQAVsbQ0vuvs";
+  static String messagingSenderId = "106064659572";
+  static String projectId = "chatappflutter-b0a3e";
+  final primaryColor = const Color(0xFFee7b64);
+}
